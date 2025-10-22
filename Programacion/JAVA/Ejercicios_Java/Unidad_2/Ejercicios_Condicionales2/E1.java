4f3cbf0b381027cdd16af9562e2b3cac450ea2ce
David-Dama <davidlopezzurron@gmail.com>
2025-10-22 13:30:33 +0200

    E1

--- Programacion/JAVA/Ejercicios_Java/Unidad_2/Ejercicios_Condicionales2/E1.java
+++ Programacion/JAVA/Ejercicios_Java/Unidad_2/Ejercicios_Condicionales2/E1.java
@@ -5,8 +5,30 @@ igual a cero.*/
 
 package Programacion.JAVA.Ejercicios_Java.Unidad_2.Ejercicios_Condicionales2;
 
+import java.util.Scanner;
+
 public class E1 {
     public static void main(String[] args) {
 
+        //Scanner y variables
+        Scanner input = new Scanner(System.in);
+        double base, altura, area;
+
+        //Entrada de datos
+        System.out.print("Introduce la base del triángulo: ");
+        base = input. nextDouble();
+        System.out.print("Introduce la altura del triángulo: ");
+        altura = input. nextDouble();
+
+        //Cálculo y salida de datos
+        if (base > 0 && altura > 0) {
+            area = (base * altura) / 2;
+            System.out.println("El área del triángulo es: " + area);
+        } else {
+            System.out.println("La base y la altura deben ser números positivos y mayores que cero.");
+
+        //Cerrar Scanner
+        input.close();
+        }
     }
 }
