0a049260507954b3c0b44c4b24a2903e3463bbd9
David-Dama <davidlopezzurron@gmail.com>
2025-10-22 14:55:09 +0200

    .

---
+++ Programacion/JAVA/Ejercicios_Java/Unidad_2/Ejercicios_Condicionales2/E7.java
@@ -0,0 +1,26 @@
+/*Escribe un programa que evalúe si una persona es elegible para un préstamo, 
+basándose en dos variables de entrada:  
+- el Salario Anual (un número real) y  
+- la Antigüedad Laboral en años (un número entero).  
+Y en las siguientes reglas: 
+- Elegible:  Si  el  Salario  Anual  es  mayor  o  igual  a  30.000  euros  Y  tiene  una 
+Antigüedad Laboral de 5 años o más. 
+- Revisión  Especial:  Si  el  Salario  Anual  es  menor  a  30.000  euros  pero  la 
+Antigüedad Laboral es de 10 años o más. 
+- No Elegible: En cualquier otro caso. 
+El  programa  debe  imprimir,  dependiendo  de  qué  reglas  de  las  anteriores  se 
+cumplan:  "Préstamo  Aprobado", "Revisión  Especial  Requerida"  o "Préstamo 
+Denegado" */
+
+package Programacion.JAVA.Ejercicios_Java.Unidad_2.Ejercicios_Condicionales2;
+
+import java.util.Scanner;
+
+public class E7 {
+    public static void main(String[] args) {
+        
+        //Importar Scanner y declarar variables
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
