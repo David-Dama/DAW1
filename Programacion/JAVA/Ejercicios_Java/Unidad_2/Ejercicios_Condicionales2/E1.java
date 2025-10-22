9c2b3dd73b4bea3944c540af3c3c97007a7191a9
David-Dama <davidlopezzurron@gmail.com>
2025-10-22 13:23:32 +0200

    subida de archivos

---
+++ Programacion/JAVA/Ejercicios_Java/Unidad_2/Ejercicios_Condicionales2/E1.java
@@ -0,0 +1,12 @@
+/*Escribe una aplicación que solicite por consola dos números reales que
+corresponden a la base y la altura de un triángulo. Deberá mostrarse su área,
+comprobando que los números introducidos por el usuario no son negativos ni
+igual a cero.*/
+
+package Programacion.JAVA.Ejercicios_Java.Unidad_2.Ejercicios_Condicionales2;
+
+public class E1 {
+    public static void main(String[] args) {
+
+    }
+}
