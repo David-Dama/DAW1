6686838300e84b7e2e1aea3f08ca9d2423847c25
David-Dama <davidlopezzurron@gmail.com>
2025-11-06 14:51:44 +0100

    Explicacion Funciones y añadido de presentacion SSII montando nuestro pc

---
+++ Programacion/JAVA/Ejercicios_Explicaciones/PruebaFunciones.java
@@ -0,0 +1,32 @@
+package Programacion.JAVA.Ejercicios_Explicaciones;
+
+public class PruebaFunciones {
+
+    public static void main(String[] args) {
+        boolean pruebaVariable = miSaludo("pepe");
+        System.out.println((pruebaVariable));//vale true
+        if(miSaludo("paco")){
+            System.out.println("Todo ha ido bien");
+        }
+    }
+
+    public static boolean miSaludo(String cadena){
+        System.out.println("¡HOLAAAA " + cadena + " desde mi función!");
+        boolean resultado;
+        if (cadena.length() < 1){
+            resultado = false;
+        } else {
+            resultado = true;
+        }
+        return resultado; //Un único punto de retorno en la función!!!
+    }   
+}
+
+/*EXPLICACIONES:
+ * Si no va a retornar nada se pone void, si en donde iría el void pone alguna clase o variable, significia que deberá devolver algo de esa variable o clase
+ * 
+ * Lo de String cadena que está dentro de la función miSaludo significa que hay que introducir un parámetro de la clase string en ese caso que se guardará en 
+ * una variable llamada cadena
+ * 
+ * erf
+ */
