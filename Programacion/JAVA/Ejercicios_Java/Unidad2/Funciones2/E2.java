f833c531319f6744149f6cc5017b44a5a25da5a5
David-Dama <davidlopezzurron@gmail.com>
2025-11-13 14:48:34 +0100

    Ejercicio E2 Funciones 2

---
+++ Programacion/JAVA/Ejercicios_Java/Unidad2/Funciones2/E2.java
@@ -0,0 +1,34 @@
+package Programacion.JAVA.Ejercicios_Java.Unidad2.Funciones2;
+
+import java.util.Scanner;
+
+public class E2 {
+    public static void main (String[] args){
+        //Declaración de variables
+        Scanner input = new Scanner(System.in);
+        int cantidadNumeros, valorMin, valorMax;
+
+        //Solicitud de datos
+        do {
+            System.out.print("Ingrese la cantidad de numeros aleatorios que desea ver: ");
+            cantidadNumeros = input.nextInt();
+            if (cantidadNumeros < 0){
+                System.out.println("Debe ingresar una cantidad de numero mayor o igual que 0.");
+            }
+        } while (cantidadNumeros >= 0);
+        System.out.print("Ingrese el mínimo: ");
+        valorMin = input.nextInt();
+        System.out.print("Ingrese el máximo: ");
+        valorMax = input.nextInt();
+
+        for (int i = cantidadNumeros; i <= cantidadNumeros; i--) {
+            System.out.println(aleatorio(cantidadNumeros, valorMin, valorMax));
+        }
+    }
+
+    public static int aleatorio(int cantidadNumeros, int valorMin, int valorMax){
+
+
+        return ;
+    }
+}
